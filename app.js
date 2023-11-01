991195f6e17ad9bc27946c10a83f169d1fe9bb8a
doniellekinchen <donicodes.things@gmail.com>
2023-11-01 16:42:32 -0400

    wed commit2

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ generateSecretCode()
 
 function selectPotions(e){
     console.log("FINALLY!", e.target.id);
+    maintable.rows[turnNumber].cells[guessNumber].style.background = e.target.id
     console.log(maintable.rows[turnNumber].cells[guessNumber]) //grab and change style.bkg property
     let currentGuess = e.target.id
     guessArray.push(currentGuess)
@@ -44,23 +45,40 @@ function selectPotions(e){
         guessArray = []
         guessNumber = 0
     }
-    let maintable = document.querySelectorAll(".maintable").style.color = "blue"
 } 
-    
-    
+
+    let number = 1; //globally scoped for checkGuess
+
+    function setCellColor(cell, color){
+        cell.style.backgroundColor = color;
+    }
+
+    function showSolution(){
+    const solutionRow = document.getElementById("solution1");
+    for (let i = 0; i < code.length; i++) {
+        setCellColor(solutionRow.cells[i], code[i]);
+    }
+}
     function checkGuess(){
         console.log('code', code)
         console.log(guessArray)
         if (JSON.stringify(code) ===  JSON.stringify(guessArray)) {
+            document.getElementById(`hint${number}`).style.backgroundColor = "green"
             console.log('correct!!')
+            showSolution();
         } else if (JSON.stringify(code) !== JSON.stringify(guessArray)) {
+            document.getElementById(`hint${number}`).style.backgroundColor = "red"
+            number++
             console.log('something is wrong') 
             turnNumber++
             if (turnNumber === 6){
-                console.log('game over') 
+                console.log('game over')
+                showSolution(); 
             }
         }
+        
     }
+
 
 //reset button
 function resetGame() {
@@ -69,14 +87,11 @@ function resetGame() {
     turnNumber = 0;
     code = []
     generateSecretCode(); // Regenerate the secret code
-    console.log('start over')
-    console.log(guessNumber)
+    console.log('start over');
+    console.log(guessNumber);
+    let box = document.querySelectorAll(".maintable tr td");
+    console.log(box)
 }
 document.getElementById("reset").onclick = function() {
     resetGame();
-};
-
-
-
-
-//if turnNumber = 6, then game over
+}
